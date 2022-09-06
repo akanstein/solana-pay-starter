825e403f1971eeb0ec6b44389cfb3566bc43e441
akanstein <akan.david.akpan@gmail.com>
2022-09-06 11:38:04 +0100

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,48 @@
 import React, { useMemo } from "react";
 import "../styles/globals.css";
 import "../styles/App.css";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 const App = ({ Component, pageProps }) => {
+  const network = WalletAdapterNetwork.Devnet;
+
+  // provision for custom rpc
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new GlowWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
 
   return (
-    <Component {...pageProps} />
+    <ConnectionProvider endpoint={endpoint} >
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 };
 
